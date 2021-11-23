a9871493caee5c3d7474b7b50cce45d3715996b9
CiCollaboration <91594924+CiCollaboration@users.noreply.github.com>
2021-11-23 18:14:55 +0900

    로그인 보내기

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log('test');
+const id = document.getElementById('id'),
+    pw = document.getElementById('pw');
+
+function login(){
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+}
